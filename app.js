54baa39d9ba83a7ecfe280cff4c90ee4ad9a9ef0
Ernandes Carvalho <ernandesdecarvalho@gmail.com>
2020-05-08 18:21:17 -0300

    updating routes

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+//const http = require('http');
+const app = express();
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+app.use(function(req, res, next) {
+    console.log('I am a custom middleware');
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.send('Hello world from express by son');
+});
+
+app.get('/world', function(req, res) {
+    res.send('World from express by son');
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(function(err, req, res, next){
+    res.status(500)
+    .json({
+        message: 'Something wrong happens'
+    });
+});
+
+app.use('/hello', routes);
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+/*http.createServer(app).listen(3000, function(req, res){
+    console.log('Hello world from express by son');
+});
+*/
+
+app.listen(3000, function() {
+    console.log('Express started');
+});
---
+++ routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+    //next(new Error('Custom error'))
+    res.json({
+        message: 'Hello world'
+    });
+});
+
+
+router.get('/params/:name', function(req, res) {
+    res.json({
+        params: req.params,
+        host: req.params,
+        headers: req.headers,
+        port: req.port
+    });
+});
+
+router.post('/body', function(req, res) {
+    res.json(req.body);
+});
+
+router.get('/res', function(req, res) {
+    res.render('index', {
+
+    });
+    /*res.status(202).json({
+        name: 'Ernandes',
+        lastname: 'carvalho'
+    });*/
+});
+
+
+module.exports = router;
